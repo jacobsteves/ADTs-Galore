0fa1544444eb1345d3d37205fe363d1c3dbd7feb
jacobsteves <jgs1998@hotmail.com>
2017-09-21 17:36:08 -0400

    clean up documentation

--- src/Inventory.c
+++ src/Inventory.c
@@ -5,28 +5,6 @@
 #include <assert.h>
 #include <stdio.h>
 
-/////////////////////////////////////////////////////////////////////////////
-// INTEGRITY INSTRUCTIONS
-
-// Explicitly state the level of collaboration on this question
-// Examples:
-//   * I discussed ideas with classmate(s) [include name(s)]
-//   * I worked together with classmate(s) in the lab [include name(s)]
-//   * Classmate [include name] helped me debug my code
-//   * I consulted website [include url]
-//   * None
-// A "None" indicates you completed this question entirely by yourself
-// (or with assistance from course staff)
-/////////////////////////////////////////////////////////////////////////////
-// INTEGRITY STATEMENT:
-// I received help from the following sources:
-
-// None
-
-// Name: Jacob Steves
-// login ID: jgsteves
-/////////////////////////////////////////////////////////////////////////////
-
 struct inventory{
   int size;
   int *qty;
